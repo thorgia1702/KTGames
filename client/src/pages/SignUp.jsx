95607def3d91ab6cb193e94820e76d0bd9142b34
thorgia1702 <97785389+thorgia1702@users.noreply.github.com>
2023-10-10 11:48:57 +0700

    create sign up page UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,42 @@
 import React from 'react'
+import './pages.css'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div>
+      <h1>Register</h1>
+      <form className="registerform">
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="username"
+            placeholder="Enter your name"
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter your email"
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter your password"
+          />
+        </div>
+
+        <button className='submitbutton' type="submit">SIGN UP</button>
+        <p className="login-register-guide">Already have an account? <a href="/sign-in">Login here!</a></p>
+      </form>
+
+    </div>
   )
 }
