7ae8bbba76b31dc83ecc080e47c7f09567494982
thorgia1702 <97785389+thorgia1702@users.noreply.github.com>
2023-10-16 15:43:23 +0700

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -18,6 +20,8 @@ app.listen(3000, () => {
 })
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
