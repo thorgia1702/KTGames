8d525102766af8aa368d8525ea25901df96d8ec2
thorgia1702 <97785389+thorgia1702@users.noreply.github.com>
2023-10-19 09:38:49 +0700

    install socket.io

--- client/src/pages/tictactoe/Board.jsx
+++ client/src/pages/tictactoe/Board.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Cell from "./Cell";
 
 export default function Board(props) {
+  
   return (
     <div className="game-board">
       {props.cells.map((item, index) => (
