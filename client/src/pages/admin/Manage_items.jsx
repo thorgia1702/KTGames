59df8b267e6dff5803ef851264bfba7a6c6fceea
thorgia1702 <97785389+thorgia1702@users.noreply.github.com>
2023-10-22 13:07:23 +0700

    complete upload item image function

--- client/src/pages/admin/Manage_items.jsx
+++ client/src/pages/admin/Manage_items.jsx
@@ -1,16 +1,99 @@
 import React, { useState } from "react";
 import "../pages.css";
-import "./admin.css"
-import { useSelector } from "react-redux";
+import "./admin.css";
 import { Button, Modal, notification } from "antd";
-import { useEffect } from "react";
-
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../../firebase";
 
 export default function Profile() {
   const [setLoading] = useState(false);
   const [open, setOpen] = useState(undefined);
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+  });
+  console.log(formData);
+  const [files, setFiles] = useState([]);
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);  
+  const showErrorModal = (errorMessage) => {
+    Modal.error({
+      title: "Error",
+      content: errorMessage,
+      centered: true,
+    });
+  };
 
+  const handleImageSubmit = (e) => {
+    e.preventDefault();
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+      setUploading(true);
+      setImageUploadError(false);
+      const promises = [];
+
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          setImageUploadError(false);
+          setUploading(false);
+        })
+        .catch((err) => {
+          setUploading(false);
+          setImageUploadError("Image upload false");
+          showErrorModal("Image upload failed");
+        });
+    } else if (files.length === 0) {
+      setUploading(false);
+      setImageUploadError("Please choose an image");
+      showErrorModal("Please choose an image");
+    } else {
+      setUploading(false);
+      setImageUploadError("You can only upload 6 images");
+      showErrorModal("You can only upload 6 images");
+    }
+  };
+
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% done`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+
+  const handleRemoveImage = (index) => {
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+    });
+  };
 
   const showModal = () => {
     setOpen(true);
@@ -27,13 +110,9 @@ export default function Profile() {
     setOpen(false);
   };
 
-
-
-
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
-  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -92,23 +171,41 @@ export default function Profile() {
                 onChange={handleChange}
               ></input>
 
-              <p>Item Image:</p>
-              <input
-                type="file"
-                className="item-information"
-                id="image"
-                onChange={handleChange}
-              ></input>
-              
+              <div>
+                <p>Item Image:</p>
+                <input
+                  type="file"
+                  className="item-information"
+                  id="image"
+                  accept="image/*"
+                  multiple
+                  onChange={(e) => setFiles(e.target.files)}
+                ></input>
+                <Button
+                  className="upload-btn"
+                  onClick={handleImageSubmit}
+                  type="button"
+                  disabled={uploading}
+                >
+                  {uploading ? 'UPLOADING...' : 'UPLOAD'}
+                </Button>
+              </div>
+              {formData.imageUrls.length > 0 &&
+                formData.imageUrls.map((url, index) => (
+                  <div key={url} className="uploaded-container">
+                    <img src={url} alt="item image" className="uploaded-img" />
+                    <Button
+                      className="delete-uploaded"
+                      type="button"
+                      onClick={()=>handleRemoveImage(index)}
+                    >
+                      DELETE
+                    </Button>
+                  </div>
+                ))}
               <br></br>
-              <Button disabled={
-            !formData.name ||
-            !formData.description ||
-            !formData.point ||
-            !formData.image
-          } className="add-btn">
-              ADD ITEM
-              </Button>
+
+              <Button className="add-btn">ADD ITEM</Button>
             </form>
           </div>
         </Modal>
