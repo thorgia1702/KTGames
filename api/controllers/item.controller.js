056403845e1cfb1b7df1f568c8c67d12900ed01a
thorgia1702 <97785389+thorgia1702@users.noreply.github.com>
2023-10-22 18:54:37 +0700

    update item route

--- api/controllers/item.controller.js
+++ api/controllers/item.controller.js
@@ -1,19 +1,19 @@
 import Item from "../models/item.model.js";
 
 export const createItem = async (req, res, next) => {
-    try {
-        const item = await Item.create(req.body);
-        return res.status(201).json(item)
-    } catch (error) {
-        next(error);
-    }
-}
+  try {
+    const item = await Item.create(req.body);
+    return res.status(201).json(item);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const getItems = async (req, res, next) => {
-    try {
-        const items = await Item.find({});
-        res.status(200).json(items);
-    } catch (error) {
-        next(error);
-    }
-}
+  try {
+    const items = await Item.find({});
+    res.status(200).json(items);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/item.route.js
+++ api/routes/item.route.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import { createItem } from '../controllers/item.controller.js';
-import { verifyToken } from '../utils/verifyUser.js';
+import express from "express";
+import { createItem, getItems } from "../controllers/item.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.post('/create',createItem)
+router.post("/create", createItem);
+router.get("/items", getItems);
 
 export default router;
